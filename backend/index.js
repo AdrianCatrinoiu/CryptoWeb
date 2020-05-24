87635d23f1186a8957b5509696770f16792fc72f
AdrianCatrinoiu <ady.catrinoiu@gmail.com>
2020-05-24 11:22:22 +0300

    stergere pagini de meniu in plus si console log-uri

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const uid = require("uid");
 //pentru scrierea in json
 const fs = require("fs");
+//pentru accesare users
 const userList = require("./databases/users.json");
 
 app.use(cors());
@@ -17,36 +18,42 @@ app.use(
 app.use(bodyParser.json());
 app.use(express.static("pages"));
 app.use(express.static("databases"));
-
+//
 app.get("/", (req, res) => {
   res.send("Tehnici Web Api");
 });
-
+//request get pentru pagina de cumparare
 app.get("/Cumparare", (req, res) => {
   res.sendFile(__dirname + "/pages/Cumparare/Cumparare.html");
 });
+//request get pentru pagina de vanzare
 app.get("/Vanzare", (req, res) => {
   res.sendFile(__dirname + "/pages/Cumparare/Vanzare.html");
 });
+//request get pentru pagina de register
 app.get("/Register", (req, res) => {
   res.sendFile(__dirname + "/pages/Register/Register.html");
 });
+//request get pentru pagina de login
 app.get("/Login", (req, res) => {
   res.sendFile(__dirname + "/pages/Login/Login.html");
 });
+//request get pentru pagina de home
 app.get("/Home", (req, res) => {
   res.sendFile(__dirname + "/pages/Home/Home.html");
 });
+//request get pentru pagina de cumparare pentru afisarea cryptomonedelor
 app.get("/crypto", (req, res) => {
   res.sendFile(__dirname + "/databases/crypto.json");
 });
+//request get pentru pagina de 404
 app.get("*", (req, res) => {
   res.status(404).sendFile(__dirname + "/Error/Error.html");
 });
-
+//request post pentru inregistrarea unui nou utilizator
 app.post("/register", (req, res) => {
-  console.log("a mers nodemon");
   console.log("req:" + JSON.stringify(req.body));
+  //cream un obiect de tip user pentru a stoca si trimite informatiile in fisierul users.json
   const newUserData = {
     id: uid(32),
     username: req.body.username,
@@ -71,7 +78,7 @@ app.post("/register", (req, res) => {
     type: "success",
   });
 });
-
+//request de verificare date login
 app.post("/loginCheck", (req, res) => {
   const { username, password } = req.body;
 
@@ -82,9 +89,9 @@ app.post("/loginCheck", (req, res) => {
   if (!password) {
     message = { message: "Parola lipsa" };
   }
-  console.log("login2");
+
   let userList = JSON.parse(fs.readFileSync("./databases/users.json", "utf8"));
-  console.log("users: " + JSON.stringify(userList));
+
   let userExists = false;
 
   userList.forEach((user) => {
